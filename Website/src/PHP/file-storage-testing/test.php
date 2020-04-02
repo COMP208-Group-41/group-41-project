bebb3b589907682bd4991f8fc486face247b1f7e
SamuelTribeUK <Samueltribeuk@outlook.com>
2020-04-02 17:01:23 +0100

    Tested file upload

---
+++ Website/src/PHP/file-storage-testing/test.php
@@ -0,0 +1,17 @@
+<?php
+    /* Testing interacting with file storage on the uni server in this file */
+    if ($_FILES['photo']['size'] == 0) {
+        die("No file selected");
+    } else {
+        $filename = $_FILES['photo']['name'];
+        echo "Stored in: " . $_FILES["photo"]["tmp_name"];
+        if (move_uploaded_file($_FILES['photo']['tmp_name'],"/home/sgstribe/private_upload/".$filename)) {
+            echo "success";
+        } else {
+            echo "error";
+        }
+    }
+
+
+
+?>
