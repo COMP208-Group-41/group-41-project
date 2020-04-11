cd202dbc41613402ca1b9bd69cb3ee3ff9049718
SamuelTribeUK <Samueltribeuk@outlook.com>
2020-04-11 11:08:41 +0100

    Started on venue-edit-user php and corrected paths to assets

--- Website/src/PHP/venue-edit-user.php
+++ Website/src/PHP/venue-edit-user.php
@@ -1,7 +1,23 @@
 <?php
+    // Session is started
+    session_start();
+
+    /* If the venue user is not logged in then redirect to venue login */
+    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+        header("location: venue-login.php");
+        exit;
+        /* If the user is logged in but they are not a venue user then they are
+         * redirected to home page
+         */
+    } else if (!isset($_SESSION["VenueUserID"])) {
+        header("location: home.php");
+        exit;
+    }
+
+    // The config file is imported here for any database connections required later
+    require_once "config.php";
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang='en-GB'>
@@ -11,7 +27,7 @@
 </head>
 <body>
 <div class="wrapper">
-    <img src="../../Assets/outout.svg" alt="OutOut">
+    <img src="../Assets/outout.svg" alt="OutOut">
     <form name='EditVenueUserDetails' method='post' style="margin-top: 10px">
         <div class="edit-fields">
             <input type='text' name='email' placeholder="Email">
