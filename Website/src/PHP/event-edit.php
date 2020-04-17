beb812114a64862df33a608634efc7b250d01c41
SamuelTribeUK <Samueltribeuk@outlook.com>
2020-04-17 12:46:44 +0100

    Added formatting for sql retrieval of datetime

--- Website/src/PHP/event-edit.php
+++ Website/src/PHP/event-edit.php
@@ -339,7 +339,7 @@
 
     // Returns an array of all event infomation
     function getEventInfo($eventID,$pdo) {
-        $getVenueStmt = $pdo->prepare("SELECT VenueID, EventName, EventDescription, EventStartTime, EventEndTime FROM Event WHERE EventID=:EventID");
+        $getVenueStmt = $pdo->prepare("SELECT VenueID, EventName, EventDescription, DATE_FORMAT(EventStartTime,'%Y-%m-%dT%H:%i') AS EventStartTime, DATE_FORMAT(EventEndTime,'%Y-%m-%dT%H:%i') AS EventEndTime FROM Event WHERE EventID=:EventID");
         $getVenueStmt->bindValue(":EventID",$eventID);
         $getVenueStmt->execute();
         return $getVenueStmt->fetch();
