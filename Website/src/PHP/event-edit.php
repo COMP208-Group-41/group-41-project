141fafe2dc10d06a453ab05b7f7a5e26ea229b4f
SamuelTribeUK <Samueltribeuk@outlook.com>
2020-04-17 11:30:05 +0100

    corrected typo

--- Website/src/PHP/event-edit.php
+++ Website/src/PHP/event-edit.php
@@ -71,7 +71,7 @@
     }
 
 
-    function checkInputs($venueUserID,$EventID,$venueID,&$errorMessage,$pdo) {
+    function checkInputs($venueUserID,$eventID,$venueID,&$errorMessage,$pdo) {
 
         // Firstly check the user's password
         if (!(isset($_POST['password']) && !empty($_POST['password']))) {
