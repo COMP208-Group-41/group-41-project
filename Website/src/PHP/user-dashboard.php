e2eeaa557527df84ea8f1356055970f28350927b
willdunnion <williamdunnion@gmail.com>
2020-04-19 16:36:36 +0100

    PHP error reporting added to dashboard

--- Website/src/PHP/user-dashboard.php
+++ Website/src/PHP/user-dashboard.php
@@ -2,6 +2,10 @@
 
   session_start();
 
+  error_reporting( E_ALL );
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+
     if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
         header("location: login.php");
         exit;
