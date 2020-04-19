78a1148603974bc4c87f3758f4441a7c78940eca
SamuelTribeUK <Samueltribeuk@outlook.com>
2020-04-19 15:07:42 +0100

    changed css link

--- Website/src/PHP/venue-user-dashboard.php
+++ Website/src/PHP/venue-user-dashboard.php
@@ -37,7 +37,7 @@
     <title>OutOut - Venue User Dashboard</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../css/venue-user-dashboard.css">
+    <link rel="stylesheet" href="../css/venue.css">
     <link rel="stylesheet" type="text/css" href="../css/navbar.css">
 </head>
 <body>
