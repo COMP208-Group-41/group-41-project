a254c6c7fffbea195791827e0ed39be37cd4430d
SamuelTribeUK <Samueltribeuk@outlook.com>
2020-04-19 20:59:13 +0100

    changed order of navbar components

--- Website/src/PHP/navbar.php
+++ Website/src/PHP/navbar.php
@@ -32,8 +32,9 @@
         <?php
             if (!isset($_SESSION['UserID']) && !isset($_SESSION['VenueUserID'])) {
                 echo '<a href="login.php">Log In</a>';
-                echo '<a href="venue-user-login.php">Venue Log In</a>';
                 echo '<a href="register.php">Register</a>';
+                echo '<br>';
+                echo '<a href="venue-user-login.php">Venue Log In</a>';
                 echo '<a href="venue-user-register.php">Register Venue</a>';
             } else {
                 echo '<a href="logout.php">Log Out</a>';
