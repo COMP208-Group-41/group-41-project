0c476b5632cafa4629a14079e98c3334aac7fd27
SamuelTribeUK <Samueltribeuk@outlook.com>
2020-04-20 14:35:27 +0100

    testing event.php times again

--- Website/src/PHP/event.php
+++ Website/src/PHP/event.php
@@ -23,8 +23,8 @@
     $venueName = $getVenueDetails['VenueName'];
     $name = $result['EventName'];
     $description = $result['EventDescription'];
-    $startTime = preg_replace("/T/"," ",(string)$result['EventStartTime']);
-    $endTime = preg_replace("/T/"," ",(string)$result['EventEndTime']);
+    $startTime = str_replace("T"," ",(string)$result['EventStartTime']);
+    $endTime = str_replace("T"," ",(string)$result['EventEndTime']);
 
     if (isset($_SESSION['UserID'])) {
         $userID = $_SESSION['UserID'];
