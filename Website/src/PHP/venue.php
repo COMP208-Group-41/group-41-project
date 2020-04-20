fedb8e50d2ab87d2f97d1cd8cadeb363b5383051
willdunnion <williamdunnion@gmail.com>
2020-04-20 13:22:02 +0100

    Venue page changes, User validation etc

--- Website/src/PHP/venue.php
+++ Website/src/PHP/venue.php
@@ -1,3 +1,29 @@
+<?php
+
+  session_start();
+
+  error_reporting( E_ALL );
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+
+  require_once "config.php";
+
+  $venueID = $_GET['venueID'];
+  $result = getVenueInfo($venueID,$pdo);
+  $owner = $result['VenueUserID'];
+  $name = $result['VenueName'];
+  $description = $result['VenueDescription'];
+  $address = $result['VenueAddress'];
+  $times = $result['VenueTimes'];
+
+  if (isset($_SESSION["UserID"])){
+    $userID = $_SESSION["UserID"];
+  } elseif (isset($_SESSION["VenueUserID"]) && $owner == $_SESSION["VenueUserID"]){
+    $venueUserID = $_SESSION["VenueUserID"];
+  }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en-GB">
 <head>
@@ -10,6 +36,9 @@
             <h1 class="title">Venue name here</h1>
             <div class="seperator"></div>
 
+            <label>Image:</label>
+            <img src="../Assets/venue-image.jpg" alt="Venue Image">
+
             <label>Location:</label>
             <label>Location here</label>
 
@@ -18,9 +47,6 @@
 
             <label>Venue description:</label>
             <textarea readonly placeholder="Description of event here"></textarea>
-
-            <label>Image:</label>
-            <img src="../Assets/venue-image.jpg" alt="Venue Image">
 
             <label style="text-align: center; margin-top: 16px;"><b>Venue Tags:</b></label>
             <div style="display: flex; justify-content: center; ">
