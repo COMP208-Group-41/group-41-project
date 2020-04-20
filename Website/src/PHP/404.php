733962d31f05216257499afeb38f6fad25ad669c
SamuelTribeUK <Samueltribeuk@outlook.com>
2020-04-20 14:45:03 +0100

    added divs to 404.php

--- Website/src/PHP/404.php
+++ Website/src/PHP/404.php
@@ -12,13 +12,18 @@
     </head>
     <body>
         <?php include "navbar.php" ?>
+        <div class="wrapper">
         <?php
             if (isset($_SESSION['message'])) {
                 echo "<div class='success'>".$_SESSION['message']."</div>";
                 unset($_SESSION['message']);
             }
         ?>
-        <h1 class='title'>Error 404 - Page Not Found!</h1>
-        <p>The page you tried to access was not found!</p>
+
+            <div class="container">
+                <h1 class='title'>Error 404 - Page Not Found!</h1>
+                <p>The page you tried to access was not found!</p>
+            </div>
+        </div>
     </body>
 </html>
