3a918f43c522eebaaf0b49950012ab97686c4a71
SamuelTribeUK <Samueltribeuk@outlook.com>
2020-04-20 13:11:44 +0100

    updated image location on event page

--- Website/src/PHP/event.php
+++ Website/src/PHP/event.php
@@ -13,18 +13,18 @@
     <div class="container">
         <div style="display: flex; flex-direction: column">
             <h1 class="title">Event name here</h1>
+            <label>Image:</label>
+            <img src="../Assets/event-image.jpg">
             <div class="seperator"></div>
             <label>Location:</label>
             <label>Location here</label>
-
 
             <label>Event time:</label>
             <label>Time here:</label>
             <label>Event description:</label>
             <textarea readonly placeholder="Description of event here"></textarea>
 
-            <label>Image:</label>
-            <img src="../Assets/event-image.jpg">
+
         </div>
     </div>
 </div>
