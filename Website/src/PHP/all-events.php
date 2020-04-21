d20ef2e4484c9ba5e4f5209e493719d0f0442bc8
SamuelTribeUK <Samueltribeuk@outlook.com>
2020-04-21 12:54:31 +0100

    added tag display to all-events

--- Website/src/PHP/all-events.php
+++ Website/src/PHP/all-events.php
@@ -52,12 +52,14 @@
             <?php
               if (sizeof($allEvents) != 0) {
                   foreach($allEvents as $row) {
+                      $currentTagIDs = getEventTagID($row['EventID'],$pdo);
                       echo '<div class="seperator" style="margin-top: 4px">';
                       echo "<table>";
                       echo "<tr>";
                       echo "<td>".$row['EventName']."</td>";
                       echo '<td><div class="venue-buttons"><a href="event.php?eventID='.$row['EventID'].'" class="venue-button" style="margin-left: -1px">View Event</a>';
                       echo '<a href="venue?venueID='.$row['VenueID'].'" class="venue-button" style="margin-right: -1px">View Venue</a></div></td>';
+                      echo '<td><div class="tag-container" style="text-align: center">'.getTagsNoEcho($currentTagIDs,$pdo).'</div></td>';
                       echo "</tr><tr>";
                       echo "<td>Event Date: ".$row['EventStartTime']."</td>";
                       echo "</tr>";
