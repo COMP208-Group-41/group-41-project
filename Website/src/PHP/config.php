6db40d520207245195cf3c127c17b4ceb0a46944
SamuelTribeUK <Samueltribeuk@outlook.com>
2020-04-21 18:59:28 +0100

    experimenting with remove of folders/files

--- Website/src/PHP/config.php
+++ Website/src/PHP/config.php
@@ -619,4 +619,20 @@ function validate255($name) {
             rmdir($path);
         }
     }
+
+    function rrmdir($dir) {
+        if (is_dir($dir)) {
+            $objects = scandir($dir);
+            foreach ($objects as $object) {
+                if ($object != "." && $object != "..") {
+                    if (is_dir($dir. DIRECTORY_SEPARATOR .$object) && !is_link($dir."/".$object)) {
+                        rrmdir($dir. DIRECTORY_SEPARATOR .$object);
+                    } else {
+                        unlink($dir. DIRECTORY_SEPARATOR .$object);
+                    }
+                }
+            }
+            rmdir($dir);
+        }
+    }
 ?>
