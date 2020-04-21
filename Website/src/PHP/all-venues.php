6065751a2dbe8a2924d15102737b0a35f9b01011
SamuelTribeUK <Samueltribeuk@outlook.com>
2020-04-21 11:07:46 +0100

    Started all-venues.php page

---
+++ Website/src/PHP/all-venues.php
@@ -0,0 +1,39 @@
+<?php
+
+    /* This will list all venues in the system */
+
+    session_start();
+
+
+
+?>
+<!DOCTYPE html>
+<html lang='en-GB'>
+<head>
+    <title>OutOut - Venues</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
+    <link rel="stylesheet" type="text/css" href="../css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="../css/dashboard.css">
+</head>
+<body>
+    <?php include "navbar.php" ?>
+    <?php
+    if (isset($_SESSION['message'])) {
+        echo "<div class='message-wrapper'><div class='success'>" . $_SESSION['message'] . "</div></div>";
+        unset($_SESSION['message']);
+    }
+    ?>
+    <div class="wrapper">
+        <div class="container">
+            <h1 class='title'>All Venues</h1>
+            <div class="seperator"></div>
+
+
+
+        </div>
+    </div>
+
+</body>
+</html>
