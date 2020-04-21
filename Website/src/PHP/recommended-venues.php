f104fa4fed2e092b3b25a306504a4c9ca1e0b441
willdunnion <williamdunnion@gmail.com>
2020-04-21 19:21:12 +0100

    Furhter implmentation of reccomendation system

--- Website/src/PHP/recommended-venues.php
+++ Website/src/PHP/recommended-venues.php
@@ -14,7 +14,25 @@
         exit;
     }
 
+    $userID = $_SESSION['UserID'];
+
     $allVenues = getAllVenues($pdo);
+    $userPrefs = getUserTags($userID,$pdo);
+    $nonSortedArray = (array) null;
+    foreach($allVenues as $row){
+      $venue = $emptyArray = (array) null;
+      $venueTags = getVenueTagID($row['VenueID'],$pdo);
+      $count = 0;
+      foreach($userPrefs as $pref){
+        if(in_array($pref, $venueTags)){
+          $count++
+        }
+      }
+      if ($count > 0){
+        array_push($venue,$count,$row['VenueID']);
+        array_push($nonSortedArray,$venue);
+      }
+    }
 
 ?>
 <!DOCTYPE html>
