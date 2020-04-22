c89d5117169068effbcb2dada0451d02edb0b050
neoreddog <dcjamb@gmail.com>
2020-04-22 17:07:08 +0100

    Tweaked padding

--- Website/src/PHP/login.php
+++ Website/src/PHP/login.php
@@ -61,8 +61,8 @@
 <!DOCTYPE html>
 <html lang='en-GB'>
 <head>
-    <link rel="stylesheet" type="text/css" href="../css/navbar.css">
     <link rel="stylesheet" type="text/css" href="../css/login-register.css">
+    <link rel="stylesheet" type="text/css" href="../css/navbar.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>OutOut - Login</title>
 </head>
