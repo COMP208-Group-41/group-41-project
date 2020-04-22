0125cc604d407726c0fa3ecf4c5cdfb9c1a10086
SamuelTribeUK <Samueltribeuk@outlook.com>
2020-04-22 16:31:12 +0100

    Putting php error reporting back into all-venues

--- Website/src/PHP/all-venues.php
+++ Website/src/PHP/all-venues.php
@@ -4,6 +4,10 @@
 
     session_start();
 
+    error_reporting( E_ALL );
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+
     require_once "config.php";
 
     $allVenues = getAllVenues($pdo);
