d04424559e8ba2b7e89d069b8da0dc9d7372557e
SamuelTribeUK <Samueltribeuk@outlook.com>
2020-04-22 16:07:40 +0100

    Changed Title of search

--- Website/src/PHP/search.php
+++ Website/src/PHP/search.php
@@ -41,7 +41,7 @@
 <!DOCTYPE html>
 <html lang='en-GB'>
 <head>
-    <title>OutOut - Matching Results</title>
+    <title>OutOut - Search Results</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="../css/main.css">
