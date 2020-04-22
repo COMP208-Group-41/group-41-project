c582cb4a612819a9b99996c7564e9e3ef2d77e9d
willdunnion <williamdunnion@gmail.com>
2020-04-22 18:27:50 +0100

    CSS Links Updated

--- Website/src/PHP/event-edit.php
+++ Website/src/PHP/event-edit.php
@@ -350,8 +350,8 @@
 <html lang='en-GB'>
 <head>
     <title>OutOut - Edit <?php echo $name; ?></title>
-    <link rel="stylesheet" type="text/css" href="../css/navbar.css">
     <link rel="stylesheet" type="text/css" href="../css/main.css">
+    <link rel="stylesheet" type="text/css" href="../css/navbar.css">
     <link rel="stylesheet" type="text/css" href="../css/event-creation.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
