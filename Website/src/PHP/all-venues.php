360de8bc67b43b093136506ab27747c160672918
SamuelTribeUK <Samueltribeuk@outlook.com>
2020-04-22 09:25:42 +0100

    Fixed rating display on all-venues

--- Website/src/PHP/all-venues.php
+++ Website/src/PHP/all-venues.php
@@ -38,7 +38,19 @@
                         $currentTagIDs = getVenueTagID($row['VenueID'],$pdo);
                         echo "<div class='venue'>";
                         echo "<div class='venue-name'>".$row['VenueName'];
-                        echo "<div class='rating-wrapper'>Rating:<div class='rating-square'>5</div></div></div>";
+                        unset($priceScore);
+                        unset($safetyScore);
+                        unset($atmosphereScore);
+                        unset($queueScore);
+                        unset($totalScore);
+                        $priceScore = getPriceScore($row['VenueID'], 1, $pdo);
+                        $safetyScore = getSafetyScore($row['VenueID'], 1, $pdo);
+                        $atmosphereScore = getAtmosphereScore($row['VenueID'], 1, $pdo);
+                        $queueScore = getQueueScore($row['VenueID'], 1, $pdo);
+                        if (!($priceScore === false || $safetyScore === false || $atmosphereScore === false || $queueScore === false)) {
+                            $totalScore = ($queueScore + $atmosphereScore + $safetyScore + $priceScore) / 4;
+                            echo "<div class='rating-wrapper'>Rating:<div class='rating-square'>$totalScore</div></div></div>";
+                        }
                         echo '<div class="venue-tags" style="text-align: center">'.getTagsNoEcho($currentTagIDs,$pdo).'</div>';
                         echo '<div class="venue-buttons"><a href="venue.php?venueID='.$row['VenueID'].'" class="venue-button" style="margin-bottom: -2px">Venue</a>';
                         echo '<a href="upcoming-events.php?venueID='.$row['VenueID'].'" class="venue-button">Events</a></div>';
