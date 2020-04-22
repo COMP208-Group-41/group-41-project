4d2a7dde343d8e2b9a679ddee893e0696a0eec9e
neoreddog <dcjamb@gmail.com>
2020-04-22 12:34:45 +0100

    css tweaks

--- Website/src/PHP/event-edit.php
+++ Website/src/PHP/event-edit.php
@@ -368,7 +368,7 @@
         <?php
             if ($image) {
                 echo '<div class="seperator"></div>';
-                echo '<img src="https://student.csc.liv.ac.uk/~sgstribe/Images/Venue/'.$venueUserID.'/'.$venueID.'/'.$eventID.'/event.jpg" alt="Event Image">';
+                echo '<img src="https://student.csc.liv.ac.uk/~sgstribe/Images/Venue/'.$venueUserID.'/'.$venueID.'/'.$eventID.'/event.jpg" alt="Event Image" class="v-image">';
             }
         ?>
         <form id='EventForm' name='EventForm' method='post' enctype="multipart/form-data">
