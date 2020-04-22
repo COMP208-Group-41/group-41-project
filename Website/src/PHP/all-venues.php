33489f32edfbe33e0116ae1f14a3049ee31c99d4
neoreddog <dcjamb@gmail.com>
2020-04-22 18:31:24 +0100

    Modified css to fix layout of mobile view

--- Website/src/PHP/all-venues.php
+++ Website/src/PHP/all-venues.php
@@ -19,9 +19,9 @@
     <title>OutOut - Venues</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="../css/main.css">
-    <link rel="stylesheet" type="text/css" href="../css/navbar.css">
     <link rel="stylesheet" type="text/css" href="../css/all-venues.css">
+    <link rel="stylesheet" type="text/css" href="../css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
 </head>
 <body>
     <?php include "navbar.php" ?>
