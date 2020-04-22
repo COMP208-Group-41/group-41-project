488888bc39a4e4ed693f13d549846e8f714e25b7
neoreddog <dcjamb@gmail.com>
2020-04-22 07:40:01 +0100

    Added Meta tag to allow for mobile styling

--- Website/src/PHP/venue.php
+++ Website/src/PHP/venue.php
@@ -71,6 +71,7 @@ $image = checkVenueImageOnServer($owner, $venueID);
     <link rel="stylesheet" type="text/css" href="../css/review.css">
     <link rel="stylesheet" type="text/css" href="../css/navbar.css">
     <link rel="stylesheet" type="text/css" href="../css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 <?php include "navbar.php" ?>
