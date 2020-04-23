214020afbca6dcadd85c2dc8f5c59a931ec7018e
neoreddog <dcjamb@gmail.com>
2020-04-23 17:46:20 +0100

    Added recommended events css

--- Website/src/PHP/recommended-venues.php
+++ Website/src/PHP/recommended-venues.php
@@ -53,6 +53,7 @@
     <title>OutOut - Recommended Venues</title>
     <link rel="stylesheet" type="text/css" href="../css/main.css">
     <link rel="stylesheet" type="text/css" href="../css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="../css/all-venues.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
