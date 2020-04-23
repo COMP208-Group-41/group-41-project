71f574e45749cccd1964fe53cb502a1e0f973727
neoreddog <dcjamb@gmail.com>
2020-04-23 19:16:06 +0100

    Set height of images

--- Website/src/PHP/home.php
+++ Website/src/PHP/home.php
@@ -82,7 +82,7 @@
                 $path = "https://student.csc.liv.ac.uk/~sgstribe/Images/Venue/".$venueUserIDforPic."/".$venueIDforPic."/venue.jpg";
                 // $mostRecommended['VenueName'] - This gives the venue name
                 /* INCLUDE IMAGE ECHO HERE */
-                echo "<img src='.$path.' class='title-img'>";
+                echo "<img src='.$path.' class='title-img' style='max-height: 300px'>";
                 echo '<a class="button" href="recommended-venues.php">View more recommended venues</a>';
             } else {
                 echo "<h2 class='title'>No venue recommendations found!</h2>";
@@ -119,7 +119,7 @@
                 $venueUserIDforPic = venueIDtoVenueUserID($venueIDforPic,$pdo);
                 $eventIDforPic = $mostRecommended['EventID'];
                 $path = "https://student.csc.liv.ac.uk/~sgstribe/Images/Venue/".$venueUserIDforPic."/".$venueIDforPic."/".$eventIDforPic."/event.jpg";
-                echo "<img src='.$path.' class='title-img'>";
+                echo "<img src='.$path.' class='title-img' style='max-height: 300px'>";
                 echo '<a class="button" href="recommended-events.php">View more recommended events</a>';
             } else {
                 echo "<h2 class='title'>No event recommendations found!</h2>";
